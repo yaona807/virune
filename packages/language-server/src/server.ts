13a47d428220861adf00ffeefc8ff48ff7269ac7
Yaona <72598968+yaona807@users.noreply.github.com>
2026-09-27 00:49:13 +0900

    chore(release): prepare v1.1.0-rc.2 publication candidate

--- packages/language-server/src/server.ts
+++ packages/language-server/src/server.ts
@@ -92,7 +92,7 @@ connection.onInitialize((params: InitializeParams): InitializeResult => {
 		},
 		serverInfo: {
 			name: 'Virune Language Server',
-			version: '1.1.0-rc.1',
+			version: '1.1.0-rc.2',
 		},
 	};
 });
